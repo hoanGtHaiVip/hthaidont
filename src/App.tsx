04c4badcaa68ba3d502d24f36ae6be33ecf5c733
thaidzvippro <113168112+hoanGtHaiVip@users.noreply.github.com>
2025-06-09 21:47:55 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 import { Github, Linkedin, Mail, MapPin, Calendar, Code, Briefcase, GraduationCap, Heart } from 'lucide-react';
 
 const ProfilePage = () => {
